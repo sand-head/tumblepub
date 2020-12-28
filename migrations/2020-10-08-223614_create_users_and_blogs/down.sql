2ba9d849ea8971e661de72d0949a5059deca7271
Justin Schweigert <justin@schweigert.dev>
2020-12-27 20:12:15 -0600

    Remove all the things, we're goin' Blazor + ASP.NET (well, in this branch anyways)

--- migrations/2020-10-08-223614_create_users_and_blogs/down.sql
+++
@@ -1,4 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE user_blogs;
-DROP TABLE users;
-DROP TABLE blogs;
--- src/database/models/mod.rs
+++
@@ -1,5 +0,0 @@
-pub use blog::Blog;
-pub use user::{User, UserBlogs};
-
-mod blog;
-mod user;
